9194b950cbf03a7ebb1d4e0930014c5f1dcde3c1
rupesh1310 <a.rupesh66@gmail.com>
2019-03-02 00:41:15 +0530

    cities added

--- js/app.js
+++ js/app.js
@@ -13,21 +13,39 @@ class ZOMATO {
 async searchAPI(city, categoryID){
 //category url
 const categoryURL = `https://developers.zomato.com/api/v2.1/categories`;
+//city url
+const cityURL = `https://developers.zomato.com/api/v2.1/cities?q=${city}`;
+
 
 //category  data
 const categoryInfo = await fetch(categoryURL,this.header);
 const categoryJSON = await categoryInfo.json();
 const categories = await categoryJSON.categories;
- 
+ //search city
+const cityInfo = await fetch(cityURL,this.header);
+const cityJSON = await cityInfo.json();
+const cityLocation = await cityJSON.location_suggestions;
+
+let cityID = 0;
+
+if (cityLocation.length > 0) {
+  cityID = await cityLocation[0].id;
+}
+//search restaurant
+
+
+
+
  return {
-    categories
+    categories,
+    cityID
   };
  }
 }
 class UI {
 constructor(){
-    this.loader = document.querySelector('.loader');
-    this.restaurantList = document.getElementById('restaurant-list');
+    this.loader = document.querySelector(".loader");
+    this.restaurantList = document.getElementById("restaurant-list");
  }
  addSelectOptions(categories){
     const search = document.getElementById("searchCategory");
@@ -39,13 +57,19 @@ constructor(){
     search.innerHTML = output;
  }
 showFeedback(text){
-const feedback = document.querySelector('.feedback');
+const feedback = document.querySelector(".feedback");
 feedback.classList.add("showItem");
 feedback.innerHTML = `<p>${text}</p>`;
 setTimeout(() => {
     feedback.classList.remove("showItem");
 }, 3000); 
-}
+ }
+showLoader(){
+    this.loader.classList.add("showItem");
+ }
+hideLoader(){
+    this.loader.classList.remove("showItem");
+ }
 }
 
 (function(){
@@ -71,11 +95,18 @@ searchForm.addEventListener("submit", event => {
     const categoryID = parseInt(searchCategory.value);
    
    if(city === '' || categoryID === 0){
-    ui.showFeedback('please enter a city and select category');
+    ui.showFeedback("please enter a city and select category");
    }
 else{
 //logic goes here
-
-}
+zomato.searchAPI(city).then(cityData => {
+    if(cityData.cityID === 0){
+        ui.showFeedback("please enter a valid city !");
+    }
+    else{
+        ui.showLoader();
+    }
+  });
+ }
 });
 })();
