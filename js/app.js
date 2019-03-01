f5224401d62728714d03d74754da99838e6a38f1
rupesh1310 <a.rupesh66@gmail.com>
2019-03-01 23:12:44 +0530

    searchapi cateogries

--- js/app.js
+++ js/app.js
@@ -1,24 +1,26 @@
 class DELL {
-constructor(){
-    this.api = "c1c92e6f53e4a41fc598b394a5c1766b";
-    this.header = {
-        method: 'GET',
-        headers:{
-            'user-key': this.api,
-            'Content-Type': 'application/json'
-        },
-        credentials:'same-origin'
-  } 
+    constructor() {
+        this.api = "c1c92e6f53e4a41fc598b394a5c1766b";
+        this.header = {
+            method: "GET",
+            headers: {
+                "user-key": this.api,
+                "Content-Type": "application/json"
+            },
+            credentials: "same-origin"
+        };
+    }
+async searchAPI(city,categoryID){
+//category url
+const categoryURL = `https://developers.zomato.com/api/v2.1/categories`;
  }
 }
-class UI {
-
-}
+class UI {}
 
 (function(){
-const searchForm = document.getElementById('searchForm');
-const searchCity = document.getElementById('searchCity');
-const searchCategory = document.getElementById('searchCate');
+const searchForm = document.getElementById("searchForm");
+const searchCity = document.getElementById("searchCity");
+const searchCategory = document.getElementById("searchCate");
 
 const DELL = new DELL();
 
@@ -26,7 +28,7 @@ const ui = new UI();
 
 
 //add select options
-document.addEventListener('DOMContentLoaded',()=>{
+document.addEventListener("DOMContentLoaded",()=>{
 //all the logics here
 })
 })();
