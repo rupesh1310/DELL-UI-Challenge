1897a8a998f90742a69ec6cb90a27ceb7c974284
rupesh1310 <a.rupesh66@gmail.com>
2019-03-01 22:57:06 +0530

    adding classes in app.js

---
+++ js/app.js
@@ -0,0 +1,22 @@
+class DELL {
+
+}
+class UI {
+
+}
+
+(function(){
+const searchForm = document.getElementById('searchForm');
+const searchCity = document.getElementById('searchCity');
+const searchCategory = document.getElementById('searchCate');
+
+const DELL = new DELL();
+
+const ui = new UI();
+
+
+//add select options
+document.addEventListener('DOMContentLoaded',()=>{
+//all the logics here
+})
+})();
